6b225ba6b198b7256d8ddb72b19f64b9114e4744
Jashimjnn <mdjashimuddinrubel8305@gmail.com>
2024-03-20 22:14:56 +0600

    mo commit

--- XPSC_Batch_4/Week1/Practise/Sale.cpp
+++ XPSC_Batch_4/Week1/Practise/Sale.cpp
@@ -4,52 +4,31 @@ using namespace std;
 typedef long long ll;
 #define Y cout << "YES" << endl;
 #define N cout << "NO" << endl;
-void fun()
-{
-    ll n;
-    cin >> n;
-    vector<ll> v(n);
-    ll idx;
-    for (int i = 0; i < n; i++)
-    {
-        cin >> v[i];
-    }
-    vector<ll> pre(n);
-    pre[n - 1] = v[n - 1];
-    for (int i = n - 2; i >= 0; i--)
-    {
-        pre[i] = v[i] + pre[i + 1];
-    }
-    ll mx = 0;
-    sort(pre.rbegin(), pre.rend());
-    for (int i = 0; i < n; i++)
-    {
-        if (i < n - 1)
-        {
-            if ((mx + 2 * v[i]) > pre[i + 1])
-            {
-                mx += 2 * v[i];
-                break;
-            }
-            else
-            {
-                mx += v[i];
-            }
-        }
-        else
-        {
-            mx += 2 * v[i];
-        }
-    }
-    cout<<mx<<endl;;
-}
 int main()
 {
     ll t;
     cin >> t;
     while (t--)
     {
-        fun();
+        ll n;
+        cin >> n;
+        vector<ll> v(n);
+        ll mx = LLONG_MIN;
+        ll idx;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> v[i];
+        }
+        for (int i = 0; i < n; i++)
+        {
+            if (mx < v[i])
+            {
+                mx = v[i];
+                idx = i;
+            }
+        }
+        ll ans = mx * 2 + (idx);
+        cout << ans << endl;
     }
     return 0;
 }
