94aa4a86aa21a3651b689218aaccf3ebe3698199
Jashimjnn <mdjashimuddinrubel8305@gmail.com>
2024-06-25 22:36:58 +0600

    m commit

---
+++ XPSC_Batch_4/Week13/PreviousSmall.cpp
@@ -0,0 +1,65 @@
+/**
+ *    author:  Mohammad Jashim Uddin
+**/
+#include <iostream>
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+typedef long long ll;
+#define Y cout << "YES" << endl;
+#define yy cout << "Yes" << endl;
+#define N cout << "NO" << endl;
+#define nn cout << "No" << endl;
+#define one cout << "1" << endl;
+#define onee cout << "-1" << endl;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int main()
+{
+    ll n;
+    cin>>n;
+    vector<ll>v(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>v[i];
+    }
+    vector<ll>next(n);
+    stack<ll>st;
+    st.push(-1);
+    for(int i=0;i<n;i++)
+    {
+        ll cur = v[i];
+        while(st.top()!=-1 && v[st.top()]>=cur)
+        {
+            st.pop();
+        }
+        next[i] = st.top();
+        st.push(i);
+    }
+    // for(int i=0;i<n;i++)
+    // {
+    //     ll cur = v[i];
+    //     while(!st.empty())
+    //     {
+    //         if(st.top()<cur)
+    //         {
+    //             next[i] = st.top();
+    //             break;
+    //         }
+    //         else
+    //         {
+    //             st.pop();
+    //         }
+    //     }
+    //     st.push(cur);
+    // }
+    for(auto x:next)
+    {
+        cout<<x<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
